6706dca43b2465d65a6e24135804437e5f00af6b
hello <hello>
2022-09-18 17:38:56 +0900

    024과제 개선 -. 영화포스터 화면표시 성공: 화면전환시 프로퍼티 생성을 통해 영화포스터 데이터 전달

--- Week6_Assignment/Week6_Assignment/ClosureViewController.swift
+++ Week6_Assignment/Week6_Assignment/ClosureViewController.swift
@@ -19,27 +19,25 @@ class ClosureViewController: UIViewController {
         showAlert(title: "컬러피커를 띄우겠습니까?", message: nil, okTitle: "띄우기")
         showAlert2(title: "컬러피커를 띄우겠습니까?", message: nil, okTitle: "띄우기") {
             let picker = UIColorPickerViewController() //UIFontPickerViewController
-            present(picker, animated: true)
-    }
+            self.present(picker, animated: true)
+        }
     }
     @IBAction func backgroundColorChanged(_ sender: UIButton) {
         showAlert(title: "배경색 변경", message: "배경색을 바꾸시겠습니까?", okTitle: "바꾸기")
-        showAlert2(title: "배경색 변경", message: "배경색을 바꾸시겠습니까?", okAction: "바꾸기")
-        
-        let alert = UIAlertController(title: "", message: "", preferredStyle: .alert)
-        let cancel = UIAlertAction(title: "취소", style: .cancel)
-        let ok = UIAlertAction(title: "확인", style: .default) { action in //액션정보 전달
-            print(action.title)
+        showAlert2(title: "배경색 변경", message: "배경색을 바꾸시겠습니까?", okTitle: "바꾸기") {
             self.view.backgroundColor = .gray
         }
-        alert.addAction(cancel)
-        alert.addAction(ok)
         
-        present(alert, animated: true)
     }
 }
 
+/*익스텐션 사용 관련
+ -. 여러번 사용할 함수코드 중복 제거
+ -. 같은 함수 실행해도 매개변수 통해 다른 alert내용 표시 가능
+ -. 탈출클로저 사용하면 외부에서 함수 실행 가능
+ */
 extension UIViewController {
+    //확인버튼 클릭시 추가기능X
     func showAlert(title: String, message: String?, okTitle: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let cancel = UIAlertAction(title: "취소", style: .cancel)
@@ -54,11 +52,13 @@ extension UIViewController {
         
         
     }
-    
-    func showAlert2(title: String, message: String?, okAction: () -> () ) { //클로져 사용
+    //확인버튼 클릭시 추가기능X(okAction눌렀을 때 okAction메서드 실행)
+    //showAlert에서는 함수마다 동일한 액션이 적용된다.(self.view.backgroundColor = .gray) 중첩함수에서 다른 액션을 실행하고 싶을 때 클로저를 사용해서 처리할 수 있다.
+    //클로저는 함수내에서 직접실행을 해야하는 탈출불가 성격이 있다. 이때 @escaping을 붙여주면 탈출가능한 인자값으로 인식되어 변수나 상수에 클로저를 대입하거나 중첩함수내부에서 사용할 수 있다.
+    func showAlert2(title: String, message: String?, okTitle: String, okAction: @escaping () -> () ) { //클로져 사용
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let cancel = UIAlertAction(title: "취소", style: .cancel)
-        let ok = UIAlertAction(title: okAction, style: .default) { action in //액션정보 전달
+        let ok = UIAlertAction(title: okTitle, style: .default) { action in //액션정보 전달
             okAction()
         }
         alert.addAction(cancel)
--- Week6_Assignment/Week6_Assignment/Network/KakaoAPIManager.swift
+++ Week6_Assignment/Week6_Assignment/Network/KakaoAPIManager.swift
@@ -15,7 +15,7 @@ class KakaoAPIManager {
     
     private init() { }
     
-    let header: HTTPHeaders = ["Authorization": "KakaoAK \(APIKey.kakao)"]
+    let header: HTTPHeaders = ["Authorization": "KakaoAK \(APIKey.kakao)"] //계속 반복될 코드이기 때문에 따로 빼서 사용해도 됨.
     
     func callRequest(type: EndPoint, query: String) {
         print(#function)
@@ -33,6 +33,8 @@ class KakaoAPIManager {
         }
     }
     
+    //json이 가지고 있는 여러 데이터 중 필요한 데이터만 뽑기 위해 탈출클로저 사용(completionHandler)
+    //인자값으로 json이 가지고 있는 데이터를 보내야하기 때문에 타입도 JSON이 된다.
     func callRequest2(type: EndPoint, query: String, completionHandler: @escaping (JSON) -> () ) {
         print(#function)
         guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
@@ -41,9 +43,8 @@ class KakaoAPIManager {
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-                print("JSON: \(json)")
                 
-                completionHandler(json)
+                completionHandler(json) //외부에서 사용할 액션 내용
                 
             case .failure(let error):
                 print(error)
