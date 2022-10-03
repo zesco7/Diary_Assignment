f6b71f58c194d9d1d79d5904259f1303e0882857
hello <hello>
2022-10-03 17:28:47 +0900

    035과제 -.네트워크통신데이터 Realm에 저장

--- Diary_Assignment/Diary_Assignment/Diary/HomeViewController.swift
+++ Diary_Assignment/Diary_Assignment/Diary/HomeViewController.swift
@@ -32,12 +32,15 @@ class HomeViewController: UIViewController {
     }()
     
     //Realm3. 정렬된 Realm데이터 담을 Results타입 배열 생성(원본데이터를 사용하기보다 프로퍼티에 대입해서 사용하는게 좋음)
-    var tasks: Results<UserDiary>!
+    var tasks: Results<UserDiary>! {
+        didSet { //didSet사용하여 데이터 변경될때마다 reloadData일괄 처리
+            tableView.reloadData()
+            print("Tasks Changed")
+        }
+    }
     
     override func viewDidLoad() {
         print(#function)
-        //Realm4. Realm데이터 정렬하여 tasks에 담기
-        tasks = localRealm.objects(UserDiary.self).sorted(byKeyPath: "diaryDate", ascending: false)
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -47,14 +50,31 @@ class HomeViewController: UIViewController {
             make.edges.equalTo(view.safeAreaLayoutGuide)
             
             navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(plusButtonClicked))
+            let sortButton = UIBarButtonItem(title: "정렬", style: .plain, target: self, action: #selector(sortButtonClicked))
+            let filterButton = UIBarButtonItem(title: "필터", style: .plain, target: self, action: #selector(filterButtonClicked))
+            
+            navigationItem.leftBarButtonItems = [sortButton, filterButton]
         }
+    }
+    
+    @objc func sortButtonClicked() {
+        tasks = localRealm.objects(UserDiary.self).sorted(byKeyPath: "regDate", ascending: false)
+    }
+    
+    @objc func filterButtonClicked() {
+        //CONTAINS[c]: 대소문자구분하지 않고 필터 가능
+        //Realm문서-CRUD-Filter Data에서 다중조건 처리방법도 확인 가능
+        tasks = localRealm.objects(UserDiary.self).filter("diaryTitle CONTAINS '1'")
     }
     
     //네비게이션 컨트롤러의 동작할때 pop을 하면 스택에서 빠져나간 뷰 컨트롤러는 메모리에서 사라지기 때문에 화면전환시 viewDidLoad가 호출되지 않으므로 viewWillAppear에서 reloadData해줘야함
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        //Realm4. Realm데이터 정렬하여 tasks에 담기
+        tasks = localRealm.objects(UserDiary.self).sorted(byKeyPath: "diaryDate", ascending: true)
+        
         print(#function) //구현이 안됐을땐 콘솔에서 코드가 실행되는지 안되는지 먼저 꼭 살피자.
-        tableView.reloadData() //Realm5. 데이터갱신하여 화면표시
+        //tableView.reloadData() //Realm5. 데이터갱신하여 화면표시
     }
     
     @objc func plusButtonClicked() {
